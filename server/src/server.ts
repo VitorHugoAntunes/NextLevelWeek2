a2072eb97aaac5af50eb7d5b9da4aeaf8632d745
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2020-08-05 22:31:27 -0300

    Criação do database e rota de criar aulas

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+/**
+ * MÉTODOS
+ *
+ * GET: Buscar ou listar alguma informacao;
+ * POST: Criar uma nova informacao;
+ * PUT: Atualizar uma informacao existente;
+ * DELETE: Deletar uma informacao existente.
+ *
+ * PARAMETROS:
+ *
+ * Corpo (Request body): Dados para criacao ou atualizacao de um registro;
+ * Route Params: Identificar qual recurso eu quero atualizar ou deletar
+ * Query Params: Paginacao, filtros, ordenacao.
+ */
+
+app.use(routes);
+
+app.listen(3333);
